f9e2faabaca1ad0b41d140d32a15f208472ba9fe
ahmadfathan <32026493+ahmadfathan@users.noreply.github.com>
2026-01-14 10:15:49 +0700

    refactor: move out implementation from header

--- FthnLabsDisplay.cpp
+++ FthnLabsDisplay.cpp
@@ -84,3 +84,68 @@ void FthnLabsDisplay::loop()
 }
 
 void FthnLabsDisplay::show() {}
+
+void FthnLabsDisplay::lightRowOfAddress(uint8_t address)
+{
+    for (uint8_t i = 0; i < config.numOfAddressPins; i++)
+    {
+        digitalWrite(config.addressPins[i], ((address >> i) & 0x01));
+    }
+}
+
+void FthnLabsDisplay::latchShiftRegToOutput()
+{
+    digitalWrite(config.latPin, HIGH);
+    digitalWrite(config.latPin, LOW);
+}
+
+void FthnLabsDisplay::oeRowsOff()
+{
+    digitalWrite(config.oePin, LOW);
+}
+
+void FthnLabsDisplay::oeRowsOn()
+{
+    digitalWrite(config.oePin, HIGH);
+}
+
+void FthnLabsDisplay::calculateOeOnTime()
+{
+    _oeOnTime = map(_brightness, 0, 100, 0, 250);
+}
+
+void FthnLabsDisplay::scanDisplay()
+{
+    uint8_t bytes;
+    uint8_t numOfRows = 4;
+
+    for (uint8_t xOffset = 0; xOffset < 32; xOffset += 8)
+    {
+        vspi->beginTransaction(SPISettings(SPI_CLK, MSBFIRST, SPI_MODE0));
+        for (uint8_t i = 0; i < 4; i++)
+        {
+            bytes = 0x00;
+            for (uint8_t x = 0; x < 8; x++)
+            {
+                uint16_t y = (currentGroup + (numOfRows * (3 - i)));
+                uint8_t state = frameBuffer8[y * width() + x + xOffset] == 255;
+                bytes = bytes | state << (7 - x);
+            }
+            vspi->transfer(~bytes);
+        }
+        vspi->endTransaction();
+    }
+
+    latchShiftRegToOutput();
+    lightRowOfAddress(currentGroup);
+
+    oeRowsOn();
+    delayMicroseconds(_oeOnTime);
+    oeRowsOff();
+
+    currentGroup++;
+    if (currentGroup > 3)
+    {
+        currentGroup = 0;
+    }
+}
--- FthnLabsDisplay.h
+++ FthnLabsDisplay.h
@@ -46,69 +46,12 @@ private:
     uint8_t _brightness = 100;
     uint8_t _oeOnTime;
 
-    void lightRowOfAddress(uint8_t address)
-    {
-        for (uint8_t i = 0; i < config.numOfAddressPins; i++)
-        {
-            digitalWrite(config.addressPins[i], ((address >> i) & 0x01));
-        }
-    }
-
-    void latchShiftRegToOutput()
-    {
-        digitalWrite(config.latPin, HIGH);
-        digitalWrite(config.latPin, LOW);
-    }
-
-    void oeRowsOff()
-    {
-        digitalWrite(config.oePin, LOW);
-    }
-
-    void oeRowsOn()
-    {
-        digitalWrite(config.oePin, HIGH);
-    }
-
-    void calculateOeOnTime() {
-        _oeOnTime = map(_brightness, 0, 100, 0, 250);
-    }
-
-    void scanDisplay()
-    {
-        uint8_t bytes;
-        uint8_t numOfRows = 4;
-
-        for (uint8_t xOffset = 0; xOffset < 32; xOffset += 8)
-        {
-            vspi->beginTransaction(SPISettings(SPI_CLK, MSBFIRST, SPI_MODE0));
-            for (uint8_t i = 0; i < 4; i++)
-            {
-                bytes = 0x00;
-                for (uint8_t x = 0; x < 8; x++)
-                {
-                    uint16_t y = (currentGroup + (numOfRows * (3 - i)));
-                    uint8_t state = frameBuffer8[y * width() + x + xOffset] == 255;
-                    bytes = bytes | state << (7 - x);
-                }
-                vspi->transfer(~bytes);
-            }
-            vspi->endTransaction();
-        }
-
-        latchShiftRegToOutput();
-        lightRowOfAddress(currentGroup);
-
-        oeRowsOn();
-        delayMicroseconds(_oeOnTime);
-        oeRowsOff();
-
-        currentGroup++;
-        if (currentGroup > 3)
-        {
-            currentGroup = 0;
-        }
-    }
+    void lightRowOfAddress(uint8_t address);
+    void latchShiftRegToOutput();
+    void oeRowsOff();
+    void oeRowsOn();
+    void calculateOeOnTime();
+    void scanDisplay();
 };
 
 #endif
